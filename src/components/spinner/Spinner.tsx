3767b21295e0f16a5646e12859f8112437c65815
massfice <adrian.marian.tomasz.larysz@gmail.com>
2025-08-07 01:58:49 +0200

    removing unused components

--- src/components/spinner/Spinner.tsx
+++
@@ -1,7 +0,0 @@
-import React, { type PropsWithChildren } from 'react';
-
-type SpinnerProps = PropsWithChildren<{}>;
-
-export const Spinner: React.FC<SpinnerProps> = () => {
-    return <div>Spinner</div>;
-};
